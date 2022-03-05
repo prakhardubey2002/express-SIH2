645821fd06235440750af5558eb35141c350e884
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-03-06 00:21:35 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 4000
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
